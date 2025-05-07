89a32c9a05be6031332f98c32a587fafb4e598f4
koplas <54645365+koplas@users.noreply.github.com>
2025-05-07 15:50:57 +0200

    Reduce reproducer

--- a.hpp
+++ a.hpp
@@ -155,12 +155,7 @@ template <class _Tp> _Tp &&__declval(int);
 template <class _Tp> _Tp __declval(long);
 #pragma GCC diagnostic pop
 
-template <class _Tp> decltype(std::__declval<_Tp>(0)) declval() noexcept {
-  static_assert(!__is_same(_Tp, _Tp),
-                "std::declval can only be used in an unevaluated context. "
-                "It's likely that your current usage is trying to extract a "
-                "value from the function.");
-}
+template <class _Tp> decltype(std::__declval<_Tp>(0)) declval() noexcept {}
 
 } // namespace __1
 } // namespace std
